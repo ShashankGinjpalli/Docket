091af8222b60bff8c3549e810e6a778bc052c916
shashankginjpalli <shashankginjpalli@10.183.93.182>
2018-06-17 13:55:23 -0700

    Adding to list and printing events works

--- TodoList.py
+++ TodoList.py
@@ -1,9 +1,30 @@
 
 
+
+todo = []
+
 while True:
+    print("\n")
     print("1: Add events")
     print("2: Remove events")
     print("3: List events")
     print("4: Exit the Program")
 
-    userInput = int(input("Please enter what you would like to do"))
+    userInput = int(input("Please enter what you would like to do: \t"))
+
+    if (userInput < 1 and  userInput> 4):
+        print("Error: Please enter a valid menu option")
+        userInput = int(input("Please enter what you would like to do: \t"))
+
+    else:
+        if(userInput == 1):
+            name = str(raw_input("Enter the name of the todo: \t"))
+            description = str(raw_input("Enter a description for the todo: \t"))
+            todo.append([name,description])
+
+        if(userInput == 3):
+            print("\n")
+            for i in todo:
+                print(i)
+
+            print("\n")
