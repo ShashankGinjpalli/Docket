bb61461770f5a2666fe5af2d096dc7168a1b1e6b
shashankginjpalli <shashankginjpalli@10.183.93.182>
2018-06-17 13:25:18 -0700

    Created storage file

--- TodoList.py
+++ TodoList.py
@@ -1,1 +1,9 @@
-print("Hello World")
+
+
+while True:
+    print("1: Add events")
+    print("2: Remove events")
+    print("3: List events")
+    print("4: Exit the Program")
+
+    userInput = int(input("Please enter what you would like to do"))
