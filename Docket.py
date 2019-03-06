87fb1c258d6ae284e31c96dd36d86c95630f95c3
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2019-03-05 19:25:38 -0800

    Implemented the Calendar Functions

--- Docket.py
+++ Docket.py
@@ -1,16 +1,40 @@
 #Shashank Ginjpalli
 
-# Creating a List for the Agenda
+# Creating a List for the Agenda and the Calendar
 
 todo = []
 calendar = []
 
-
+# function used to
 def mainMenu():
     print("\n")
-    print("1: Add to the Todo List")
-    print("2: Add to the Calendar")
+    print("1: Todolist Menu")
+    print("2: Calendar Menu")
     print("3: Exit the Program")
+
+    userInput = int(input("Please enter which Menu you would like to access: "))
+    if(userInput == 1):
+        todoList()
+    if(userInput == 2):
+        calendarEvent()
+    if(userInput == 3):
+        f = open("todo.txt", "w+")
+        for i in todo:
+            f.write(str(i[0]) +","+ str(i[1])+ "\n")
+        f.close()
+
+        f2 = open("calendar.txt", "w+")
+        for i in calendar:
+            f2.write(str(i[0]) + ","+ str(i[1]) +"," + str(i[2] + "\n"))
+        f2.close()
+
+        exit()
+
+    else:
+        print("Error enter a valid menu option")
+        mainMenu()
+
+
 
 def todoList():
     # repeats the menu until the user exits the program
@@ -39,6 +63,7 @@ def todoList():
                 todo.append([name,description])
 
             if(userInput == 2):
+
                 removeIndex = int(input("Select Which Event # you would like to remove from the list:"))
 
                 # deleting the event from the todo list
@@ -60,13 +85,7 @@ def todoList():
             #saving to the file before exiting the program
             if(userInput == 4):
 
-                f = open("todo.txt", "w+")
-                for i in todo:
-                    f.write(str(i[0]) +","+ str(i[1])+ "\n")
-
-                f.close()
-
-                exit()
+                mainMenu()
 
             # deletes the list events that are stored in the file
             if(userInput == 5):
@@ -82,8 +101,57 @@ def todoList():
                     todo.pop()
 
 
-def main():
+def calendarEvent():
+    while True:
+        print("\n")
+        print("1: Add Event to Calendar")
+        print("2: Remove an event from the Calendar")
+        print("3: List Events")
+        print("4: Exit to Main Menu")
 
+        userInput = int(input("Enter what you would like to do: "))
+
+        while(userInput < 1 or userInput > 4):
+            print("Error: Enter a valid Menu option!")
+            userInput = int(input("Enter what you would like to do: "))
+
+        if(userInput == 1):
+            print("\n")
+            title = str(input("Enter a Title for the Event: "))
+            description = str(input("Enter a Description for the Event: "))
+            date = str(input("Enter a Date for the Event: "))
+
+            calendar.append([title, description, date])
+
+        if(userInput == 2):
+            print("\n")
+            remove = str(input("Enter the Name of the Event that You would like to remove"))
+            x = 0
+            for i in calendar:
+                if(remove == i[0]):
+                    calendar.remove(x)
+                    break
+            x = x + 1
+
+        if(userInput == 3):
+            print("\n")
+
+            for i in calendar:
+
+                print("Name: \t\t\t\t" + i[0])
+                print("Description: \t\t\t" + i[1] + "\n")
+                print("Date: \t\t\t\t" + i[2] + "\n")
+
+            print("\n")
+
+        if(userInput == 4):
+            mainMenu()
+
+
+
+
+
+def main():
 
     # opens a file in order to read saved events
     f = open("todo.txt", "r+")
